857637389c1a2e44bec21b1164b0775ba3a74aa3
Joshua Tyler-Jones <joshua@tylerjon.es>
2019-06-12 02:33:00 +0100

    Switch to firebase; move some imports around

--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ import VueRouter from 'vue-router';
 Vue.use(VueRouter);
 
 // Other libraries
+import 'bootstrap/dist/js/bootstrap.min.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import * as firebase from 'firebase/app';
+Vue.prototype.$firebase = firebase;
+
 import _ from 'lodash';
 Vue.prototype.$_ = _;
 
